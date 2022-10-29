38afa78027a90a5202a1027162cb5059ad847f78
Sam Lin <maxisam@gmail.com>
2022-10-29 15:28:56 -0500

    update

--- pkg/config/modules.go
+++ pkg/config/modules.go
@@ -2,9 +2,9 @@ package config
 
 type ModuleConfig struct {
 	AWSClient    bool `envconfig:"en_aws_cli" default:"false"`
-	AzureClient  bool `envconfig:"en_azure" default:"true"`
-	GCloudClient bool `envconfig:"en_gcloud" default:"true"`
-	GnuPG        bool `envconfig:"en_gpg" default:"true"`
-	MinioClient  bool `envconfig:"en_minio" default:"true"`
-	RCloneClient bool `envconfig:"en_rclone" default:"true"`
+	AzureClient  bool `envconfig:"en_azure" default:"false"`
+	GCloudClient bool `envconfig:"en_gcloud" default:"false"`
+	GnuPG        bool `envconfig:"en_gpg" default:"false"`
+	MinioClient  bool `envconfig:"en_minio" default:"false"`
+	RCloneClient bool `envconfig:"en_rclone" default:"false"`
 }
